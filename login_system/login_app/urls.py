7b0dc59de4515260e2e59ee328c05086e0e5cd0e
Larissa <larissa.queiroz@nepen.org.br>
2022-03-14 21:49:44 -0300

    first commit

---
+++ login_system/login_app/urls.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('signup', views.signup, name = 'signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('unsign', views.delete, name= 'unsign')
+]
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ login_system/login_system/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'empresa.mail.teste@gmail.com'
+EMAIL_HOST_PASSWORD = '66990609'
+EMAIL_PORT = 587
